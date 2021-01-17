cd5c9e321a6976ee87fa8b93538cfdb44835732e
amankesharwani1 <34089824+amankesharwani1@users.noreply.github.com>
2021-01-17 16:51:30 +0530

    Update Unit_test_xml_parser.py

--- Unit_test_xml_parser.py
+++ Unit_test_xml_parser.py
@@ -1,4 +1,6 @@
-import csv
+#!/usr/bin/env python
+# This is unit test case written to validate the count of records fetched after parsing the xml file to csv.
+# To Test the result pytest has been used and screenshot capturing the validation has been uploaded.
 def test_xml_parser():
     # reading the CSV file 
     row_count = sum(1 for line in open('Output_Data.csv',encoding="utf8"))
